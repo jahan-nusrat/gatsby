fa2404a8a406c182456c5a62ceed6fbb2007ec1c
nusrat-jahan <njnusrat53@gmail.com>
2020-12-12 10:52:10 +0600

    Page Query

--- src/pages/example.js
+++ src/pages/example.js
@@ -3,8 +3,11 @@ import { Helmet } from 'react-helmet'
 import Layout from '../components/Layout'
 import Header from '../example/Header'
 import HeaderStatic from '../example/HeaderStatic'
+import { graphql } from "gatsby"
 
-const example = () => {
+
+const example = (props) => {
+    const {data} = props;
     return (
         <Layout>
             <Helmet>
@@ -13,8 +16,25 @@ const example = () => {
             <h2 className="text-center">I am from Example page</h2>
             <Header />
             <HeaderStatic />
+            <h3 className="text-center text-danger">{data.site.info.title}</h3>
         </Layout>
     )
 }
+
+export const query = graphql`
+    {
+        site {
+            info: siteMetadata {
+                title
+                description
+                siteUrl
+                media {
+                    name
+                    url
+                }
+            }
+        }
+    }
+`
 
 export default example
